4d2305a5f793dfcbad3b11d3a775432db2892141
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2025-06-11 20:53:07 -0300

    🎉 Start project

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite';
+import { federation } from '@module-federation/vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    federation({
+      name: 'goals',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './goals-app': './src/App/index.tsx',
+      },
+      shared: ['react', 'react-dom'],
+    }),
+  ],
+  server: {
+    port: 3006,
+  },
+  resolve: {
+    alias: {
+      '@domain': path.resolve(__dirname, 'src/domain'),
+      '@usecases': path.resolve(__dirname, 'src/usecases'),
+      '@App': path.resolve(__dirname, 'src/App'),
+      '@fb': path.resolve(__dirname, 'src/firebase'),
+    },
+  },
+})
